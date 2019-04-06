e228612f7d0bbcc84c3374df514c9c57ed9012af
@XYZ <34183912+PASUNX@users.noreply.github.com>
2019-04-07 06:36:41 +0700

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,7 +1,7 @@
 class tools:
     def removeFile(fileName):
         if os.path.isfile(fileName):
-             os.remove(fileName)
+            os.remove(fileName)
     def restartScript():
         python = sys.executable
-             os.execl(python, python, *sys.argv)
+        os.execl(python, python, *sys.argv)
