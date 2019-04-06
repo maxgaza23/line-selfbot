eec3c8071c5fcdcd9875c51ef03ed392e040fcc0
@XYZ <34183912+PASUNX@users.noreply.github.com>
2019-04-07 06:47:45 +0700

    Delete tools.py

--- tools.py
+++
@@ -1,7 +0,0 @@
-class tools:
-    def removeFile(fileName):
-        if os.path.isfile(fileName):
-            os.remove(fileName)
-    def restartScript():
-        python = sys.executable
-        os.execl(python, python, *sys.argv)
